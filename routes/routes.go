f638f6e4ae6732618423ba725501a7ae2eb7393e
abu-sayem <abusaayem@gmail.com>
2021-09-12 11:51:19 +0600

    Add routers

---
+++ routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"salesdashboard/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/other", controllers.Other)
+}

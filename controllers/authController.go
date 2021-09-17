5791af7f76f491be25b6b2e4d346ac10f3b7c0a2
abu-sayem <abusaayem@gmail.com>
2021-09-17 08:10:43 +0600

    Add use helper function

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,11 +2,11 @@ package controllers
 
 import (
 	"salesdashboard/database"
+	"salesdashboard/utils"
 	"salesdashboard/models"
 	"strconv"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -61,11 +61,8 @@ func Register(c *fiber.Ctx) error {
 				"message": "Incorrect password",
 			})
 		}
-		claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-			Issuer: strconv.Itoa(int(user.Id)),
-			ExpiresAt: time.Now().Add(time.Hour*24).Unix(),
-		})
-		token, err := claims.SignedString(([]byte("secret")))
+
+		token, err := utils.GenerateJwt(strconv.Itoa(int(user.Id)))
 
 		if err != nil {
 			return c.SendStatus(fiber.StatusInternalServerError)
@@ -85,28 +82,14 @@ func Register(c *fiber.Ctx) error {
 		})
 	}
 
-	type claims struct {
-		jwt.StandardClaims
-	}
-
 func User(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
-	token, err := jwt.ParseWithClaims(cookie, &claims{}, func(token *jwt.Token) (interface{}, error){
-		return []byte("secret"), nil
-	})
 
-	if err != nil || !token.Valid {
-		c.Status(fiber.StatusUnauthorized)
-		return c.JSON(fiber.Map{
-			"message": "unauthnicated user",
-		})
-	}
-
-	claims := token.Claims.(*claims)
+	id, _ := utils.ParseJwtCookie(cookie)
 
 	var user models.User
 
-	database.DB.Where("id = ?", claims.Issuer).First(&user)
+	database.DB.Where("id = ?", id).First(&user)
 	return c.JSON(user)
 }
 

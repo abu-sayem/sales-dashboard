26f949e02db4f94dcebfe2352bc92f10af4fa9dc
abu-sayem <abusaayem@gmail.com>
2021-09-17 08:24:56 +0600

    Add methods in user

--- models/user.go
+++ models/user.go
@@ -1,9 +1,20 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id uint `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 	Email string `gorm:"unique" json:"email"`
 	Password []byte `json:"-"`
+}
+
+func (u *User) SetPassword(password string) {
+	hashpassword, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	u.Password = hashpassword
+}
+
+func (u *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword(u.Password, []byte(password))
 }

c6fff9ea51245e90c655eb297db0ff712250e8e8
abu-sayem <abusaayem@gmail.com>
2021-09-17 08:08:59 +0600

    Add new routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"salesdashboard/controllers"
+	"salesdashboard/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,7 +10,12 @@ import (
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	app.Use(middleware.IsAuthnicated)
+
 	app.Post("api/logout", controllers.Logout)
-	app.Get("api/other", controllers.Other)
 	app.Get("api/user", controllers.User)
+
+	app.Get("api/users", controllers.AllUser)
+	app.Post("api/users", controllers.CreateUser)
 }

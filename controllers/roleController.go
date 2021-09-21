5c0aae46b8718cfeeab917c37bbd9e6d35f113b6
abu-sayem <abusaayem@gmail.com>
2021-09-21 13:48:08 +0600

    Create roles models and crud operation

---
+++ controllers/roleController.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"salesdashboard/database"
+	"salesdashboard/models"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AllRoles(c *fiber.Ctx) error {
+		var roles []models.Role
+		database.DB.Find(&roles)
+		return c.JSON(roles)
+	}
+
+
+func CreatRole(c *fiber.Ctx) error {
+	var role models.Role
+
+	if err := c.BodyParser(&role); err != nil{
+		return err
+	}
+
+	database.DB.Create(&role)
+
+	return c.JSON(role)
+}
+
+func GeteRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	role := models.Role {
+		Id: uint(id),
+	}
+	database.DB.Find(&role)
+
+	return c.JSON(role)
+}
+
+func UpdateRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	role := models.Role {
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&role); err != nil{
+		return err
+	}
+
+	database.DB.Model(&role).Updates(role)
+
+	return c.JSON(role)
+}
+
+func DeleteRole(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	role := models.Role {
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&role)
+
+	return nil
+}
--- routes/routes.go
+++ routes/routes.go
@@ -20,5 +20,11 @@ func Setup(app *fiber.App) {
 	app.Post("api/users", controllers.CreateUser)
 	app.Get("api/users/:id", controllers.GeteUser)
 	app.Put("api/users/:id", controllers.UpdateUser)
-	app.Delete("api/users/:id", controllers.UpdateUser)
+	app.Delete("api/users/:id", controllers.DeleteUser)
+
+	app.Get("api/roles", controllers.AllRoles)
+	app.Post("api/roles", controllers.CreatRole)
+	app.Get("api/roles/:id", controllers.GeteRole)
+	app.Put("api/roles/:id", controllers.UpdateRole)
+	app.Delete("api/roles/:id", controllers.DeleteRole)
 }

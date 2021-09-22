51ddc067519dfa747426fa7f69dc83f802e38101
abu-sayem <abusaayem@gmail.com>
2021-09-22 12:12:52 +0600

    Add m2m permission to role model

--- controllers/roleController.go
+++ controllers/roleController.go
@@ -16,10 +16,25 @@ func AllRoles(c *fiber.Ctx) error {
 
 
 func CreatRole(c *fiber.Ctx) error {
-	var role models.Role
+	var roleDTO fiber.Map
 
-	if err := c.BodyParser(&role); err != nil{
+	if err := c.BodyParser(&roleDTO); err != nil{
 		return err
+	}
+
+	list := roleDTO["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+
+	role := models.Role{
+		Name: roleDTO["name"].(string),
+		Permissions: permissions,
 	}
 
 	database.DB.Create(&role)
@@ -33,7 +48,7 @@ func GeteRole(c *fiber.Ctx) error {
 	role := models.Role {
 		Id: uint(id),
 	}
-	database.DB.Find(&role)
+	database.DB.Preload("permissions").Find(&role)
 
 	return c.JSON(role)
 }
@@ -41,12 +56,28 @@ func GeteRole(c *fiber.Ctx) error {
 func UpdateRole(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 
-	role := models.Role {
-		Id: uint(id),
+	var roleDTO fiber.Map
+	if err := c.BodyParser(&roleDTO); err != nil{
+		return err
 	}
 
-	if err := c.BodyParser(&role); err != nil{
-		return err
+	list := roleDTO["permissions"].([]interface{})
+	permissions := make([]models.Permission, len(list))
+
+	for i, permissionId := range list {
+		id, _ := strconv.Atoi(permissionId.(string))
+		permissions[i] = models.Permission{
+			Id: uint(id),
+		}
+	}
+	var result interface{}
+
+	database.DB.Table("role_permissions").Where("role_id", id).Delete(&result)
+
+	role := models.Role{
+		Id: uint(id),
+		Name: roleDTO["name"].(string),
+		Permissions: permissions,
 	}
 
 	database.DB.Model(&role).Updates(role)

96d97e8f0e82d4f1fb8b1fbc71cfec26251fba10
abu-sayem <abusaayem@gmail.com>
2021-09-25 01:15:00 +0600

    Add login page

---
+++ frontend/components/LoginComponent.jsx
@@ -0,0 +1,66 @@
+import React, { Component, useState, SyntheticEvent } from "react";
+
+import { useRouter } from "next/router";
+
+import axios from "axios";
+
+const LoginComponent = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await axios.post(
+      "http://localhost:8000/api/login",
+      {
+        email,
+        password,
+      },
+      { withCredentials: true }
+    );
+
+    setRedirect(true);
+  };
+
+  if (redirect) {
+    router.push("/");
+  }
+
+  return (
+    <main className="form-signin d-flex align-items-center justify-content-center my-auto">
+      <form onSubmit={submit}>
+        <h1 className="h3 mb-3 fw-normal">Please Sig n In</h1>
+        <input
+          className="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign In
+        </button>
+        <p className="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+      </form>
+    </main>
+  );
+};
+
+export default LoginComponent;
---
+++ frontend/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import LoginComponent from "../../components/LoginComponent";
+
+class Login extends Component {
+  render() {
+    return <LoginComponent />;
+  }
+}
+
+export default Login;

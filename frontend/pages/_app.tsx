2804deafd23488322bccf8bd0b123735c8027e54
abu-sayem <abusaayem@gmail.com>
2021-09-25 12:54:09 +0600

    Add axios base url

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:8000/api/";
+axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />

60c713b7399098a6da84a0236529e1cc52d23220
abu-sayem <abusaayem@gmail.com>
2021-09-27 16:56:27 +0600

    Add redirect to login page for unauthnicated user

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -8,23 +8,21 @@ import Menu from "./Menu";
 import Nav from "./Nav";
 
 const Layout = ({ children }: { children: any }) => {
-  
-  const [redirect, setRedirect] = useState(false)
-  const router = useRouter()
+  const [redirect, setRedirect] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
       try {
-      const {data} = await axios.get("user");
-      }
-      catch(e){
-        setRedirect(true)
+        const { data } = await axios.get("user");
+      } catch (e) {
+        setRedirect(true);
       }
     })();
   }, []);
 
   if (redirect) {
-     router.push("/login");
+    router.push("/login");
   }
 
   return (
@@ -36,6 +34,8 @@ const Layout = ({ children }: { children: any }) => {
           <div className="row">
             <Menu />
             <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <div className="pt-3 pb-2 mb-3 border-bottom">
+              </div>
               {children}
             </main>
           </div>
